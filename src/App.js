ee39f02bdd7e30bbe632e2b268db54e5b9b11180
Raed2023 <raedbenabdallah25@gamil.com>
2023-02-08 12:00:25 +0100

    first-reactjs

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Form } from 'react-bootstrap';
+import Forme from './components/forme';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello world</h1>
+      <Forme/>
+    </div>
+  );
+}
+
+export default App;
